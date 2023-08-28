09b961b3bb8f576a4733838c6ee076111c61ca8d
Noini0328 <84901149+Noini0328@users.noreply.github.com>
2023-08-28 16:54:27 +0900

    Add files via upload

--- ConfirmMessage.py
+++ ConfirmMessage.py
@@ -1,5 +1,9 @@
+import sys
+from art import *
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import filedialog
+
 
 class ConfirmMessage:
     title = None
@@ -10,14 +14,28 @@ class ConfirmMessage:
             print("Confirm Yes.")
         else:
             print("Confirm No.")
+            print("Type any key to exit the program...")
+            input()
+            sys.exit()
 
 
 if __name__ == "__main__":
+    tprint("Tools Name",font="rnd-medium")
+
     root = tk.Tk()
     root.withdraw()  # hide main window
     #confirm_execution()
 
     Confirm1st = ConfirmMessage()
     Confirm1st.title = "Confirmation"
-    Confirm1st.message = "Developed by sakai(kanagawa). \nPlease run this program at your own responsibility.\nBe sure to check the evidence against the analysis results and report any bugs.\nDo you agree to the above and proceed with the execution?"
+    Confirm1st.message = "Developed by xxxx(xxxx). \nPlease run this program at your own responsibility.\nBe sure to check the evidence against the  results and report any bugs.\nDo you agree to the above and proceed with the execution?"
     Confirm1st.confirm()
+
+
+    input_file = filedialog.askopenfilename(title="入力ファイルを選択してください", filetypes=[("すべてのファイル", "*.*")])
+    print(input_file)
+
+    input_fold = filedialog.askdirectory(title="入力フォルダを選択してください")
+    print(input_fold)
+
+
